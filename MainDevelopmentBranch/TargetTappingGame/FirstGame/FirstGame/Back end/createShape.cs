9ad8ec7f205123c5df7111a7b4a71709b8b58d8a
Patrick Boutet <pboutet@ualberta.ca>
2013-03-04 14:00:35 -0700

    backend skeleton classes

---
+++ MainDevelopmentBranch/TargetTappingGame/FirstGame/FirstGame/Back end/createShape.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace FirstGame.Back_end
+{
+
+    /*
+    * This class contains all the properties (such as color, size, etc..) for our shapes,
+    * letter, numbers that the therapist will put in the level.
+     */
+
+    class createShape
+    {
+
+        //Put private class variables here.
+        int color;
+        int size;
+        string shape;
+        bool multiSelect;
+        int[][] position;
+         
+
+
+
+        //Constructor for this class
+        public createShape(string shapePassedIn, int colorPassedIn, int sizePassedIn, bool multiselectPassedIn, Array )
+        {
+            color = colorPassedIn;
+            shape = shapePassedIn;
+            size = sizePassedIn;
+            multiSelect = multiselectPassedIn;
+            
+
+        }
+
+
+        //Methods for this class, such as getters and setters, etc...
+        public void setColor(){
+            
+
+        }
+
+
+    }
+
+}
+
+   
+
+
---
+++ MainDevelopmentBranch/TargetTappingGame/FirstGame/FirstGame/Back end/drawShape.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstGame.Back_end
+{
+    /*
+    * This class draws a bitmap of an object that it takes as an argument.
+    */
+    class drawShape
+    {
+        //Put private class variables here.
+        //int = myInt;
+
+         //Constructor for this class
+        public drawShape()
+        {
+           
+            
+
+        }
+
+
+        //Methods for this class, such as getters and setters, etc...
+        // public void myMethod(){}
+
+
+
+    }
+}
