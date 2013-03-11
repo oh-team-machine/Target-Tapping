9820f503e85fe1b450913a09a64e217ea7d076d9
Aaron Padlesky <padlesky@ualberta.ca>
2013-03-10 18:53:03 -0600

    Added Create and Draw shape too Pats

--- PatsScratchwork/BackendDevelopment/BackendDevelopment/BackendDevelopment/BackEnd/CreateShape.cs
+++ PatsScratchwork/BackendDevelopment/BackendDevelopment/BackendDevelopment/BackEnd/CreateShape.cs
@@ -2,10 +2,94 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BackendDevelopment.BackEnd;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace BackendDevelopment.BackEnd
 {
     class CreateShape
     {
+        //Put private class variables here.
+        Color color;
+        int size;
+        string shape;
+        bool multiSelect;
+        int[] position = new int[2];
+         
+
+        
+
+        //Constructor for this class
+        public CreateShape(string shapePassedIn, Color colorPassedIn, int sizePassedIn, int[] positionPassedIn,
+                                bool multiSelectPassedIn)
+        {
+            setColor(colorPassedIn);
+            setShape(shapePassedIn);
+            setSize(sizePassedIn);
+            setPosition(positionPassedIn);
+            setMultiSelect(multiSelectPassedIn);
+
+        }
+
+
+        //Draws the shape on the screen.
+        public void drawShape(GraphicsDeviceManager graphics)
+        {
+            Texture2D texture;
+            DrawShape draw = new DrawShape();
+            texture = draw.drawShape(shape, size, graphics);
+        }
+
+        //Methods for this class, such as getters and setters, etc...
+        public void setColor(Color colorPassedIn)
+        {
+            color = colorPassedIn;
+        }
+
+        public void setShape(string shapePassedIn)
+        {
+            shape = shapePassedIn;
+        }
+
+        public void setSize(int sizePassedIn)
+        {
+            size = sizePassedIn;
+        }
+
+        public void setPosition(int[] poistionPassedIn)
+        {
+            position = poistionPassedIn;
+        }
+
+        public void setMultiSelect(bool multiSelectPassedIn)
+        {
+            multiSelect = multiSelectPassedIn;
+        }
+
+        public Color getColor()
+        {
+            return color;
+        }
+
+        public string getShape()
+        {
+            return shape;
+        }
+
+        public int getSize()
+        {
+            return size;
+        }
+
+        public int[] getPosition()
+        {
+            return position;
+        }
+
+        public bool getMultiSelect()
+        {
+            return multiSelect;
+        }
     }
 }
