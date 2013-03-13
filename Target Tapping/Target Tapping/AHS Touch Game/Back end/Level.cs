c140d20779fcdea169bed0a93e2f38f7d05343b5
Aaron Padlesky <padlesky@ualberta.ca>
2013-03-13 15:11:40 -0600

    Added a level class
    This level class holds onto the information of the objects to be drawn
    on screen. It has a simple add and remove method as well as a
    constructor for the class.

--- Target Tapping/Target Tapping/AHS Touch Game/Back end/Level.cs
+++ Target Tapping/Target Tapping/AHS Touch Game/Back end/Level.cs
@@ -7,7 +7,49 @@ namespace TargetTapping.Back_end
 {
     class Level
     {
-        public List<List<TargetTapping.Back_end.Object>> objectList { get; set; }
+        public Level () {
+            objectList = new List<List<Object>>();
+            currentPosition = 0;
+            multiSelect = false;
+            upTime = 0;
+            holdTime = 0;
+        }
 
+        public List<List<TargetTapping.Back_end.Object>> objectList { get; set; }
+        public int currentPosition { get; set; }
+        public bool multiSelect { get; set; }
+        public int upTime { get; set; }
+        public int holdTime { get; set; }
+
+        public void addObject(TargetTapping.Back_end.Object objectPassed)
+        {
+            if (multiSelect == true)
+            {
+                objectList[currentPosition].Add(objectPassed);
+            }
+            else
+            {
+                currentPosition = currentPosition + 1;
+                objectList[currentPosition].Add(objectPassed);
+            }
+        }
+
+        public void removeObject()
+        {
+            int lastObjectPosition = -1;
+            foreach (var objectB in objectList[currentPosition])
+        	{
+                lastObjectPosition = lastObjectPosition + 1;
+        	}
+            if (lastObjectPosition == 0)
+            {
+                objectList[currentPosition].RemoveAt(lastObjectPosition);
+                currentPosition = currentPosition - 1;
+            }
+            else
+            {
+                objectList[currentPosition].RemoveAt(lastObjectPosition);
+            }
+        }
     }
 }
