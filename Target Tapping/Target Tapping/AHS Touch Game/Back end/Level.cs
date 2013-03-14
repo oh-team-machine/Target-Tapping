a321c17eac7591d51e0136905d1f42e0ec240d60
Aaron Padlesky <padlesky@ualberta.ca>
2013-03-13 18:28:01 -0600

    Fixed bug in Level.cs
    bug was fixed from correcting how list of list was called.

--- Target Tapping/Target Tapping/AHS Touch Game/Back end/Level.cs
+++ Target Tapping/Target Tapping/AHS Touch Game/Back end/Level.cs
@@ -24,15 +24,20 @@ namespace TargetTapping.Back_end
 
         public void addObject(TargetTapping.Back_end.Object objectPassed)
         {
+            List<TargetTapping.Back_end.Object> placeHolder = new List<TargetTapping.Back_end.Object>();
+            placeHolder.Add(objectPassed);
             if (multiSelect == true)
             {
-                objectList[currentPosition].Add(objectPassed);
+                objectList.Add(placeHolder);
+            }
+            else if (currentPosition == 0)
+            {
+                objectList.Add(placeHolder);
             }
             else
             {
                 currentPosition = currentPosition + 1;
-                // AARON, IT'S BROKEN HERE: argument out of range exception!
-                objectList[currentPosition].Add(objectPassed);
+                objectList.Add(placeHolder);
             }
         }
 
