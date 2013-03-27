945245496ab524d1db47f260b1249f4b290e9fdc
Shawn Adam <saadam@ualberta.ca>
2013-03-27 15:55:03 -0600

    Writing Help

--- Target Tapping/Target Tapping/AHS Touch Game/Screens/HomeHelpScreen.cs
+++ Target Tapping/Target Tapping/AHS Touch Game/Screens/HomeHelpScreen.cs
@@ -34,12 +34,39 @@ namespace TargetTapping.Screens
             answerBackDrop = Content.Load<Texture2D>("HELP/answerBackDrop");
             answerBackDropPosition = (new Vector2(((ScreenWidth / 2) - 400), 110));
 
+            //
+            //Below are the hard-coded answers for help topics.
+            //
             font = Content.Load<SpriteFont>("font");
-            newLayoutHelpAnswer = "Choose new layout if you would like to ";
+            ///////////////////////////////////////////////////CUT OFF HERE////////
+            newLayoutHelpAnswer =
+                "QUICK ANSWER:\nTap new layout if you would like to design a new\n" +
+                "'Target-Tapping' environment for a patient.\n\n" +
+                "LONG ANSWER:\n" +
+                "As a therapist, your role is to design 'tapping environments' for\n" +
+                "patients. Patients will then play the games you make for them.\n" +
+                "To simplify the process of creating a 'tapping environment' we allow\n" +
+                "you to save the tapping environments you create.\n" +
+                "Enter a description of the game you intend to create so that in the \n" +
+                "future you will be able to identify the 'tapping environment'. In\n" +
+                "addition associate a name to the tapping environment. Name can be yours'\n" +
+                "as the therapist, or it could be the patients name that the environment\n" +
+                "is is intended for. Either way just name and describe your environments\n" +
+                "in a way that will allow them to be identified in upcoming sessions.\n\n" +
+                "E.G. Description: forty_blue_squares\n"+
+                "     Name:        sydney_crosby\n" +
+                "This will be saved as sydney_crosby_forty_blue_squares.\n" +
+                "The important thing to note is for productivity make new environments \n" + 
+                "when a current like-environment does not already exist. When \n" +
+                "like-environments you may want to load them, instead of making a new\n" + 
+                "environment.\n";
+
             loadLayoutHelpAnswer = "Choose load layout if you would like to";
             exitHelpAnswer = "The EXIT GAME button will return you to windows.\n Any unsaved progress will be lost.";
             gameBreakDownHelpAnswer = "This game is designed both for therapists and patients.";
-	       // Load buttons 'n' stuff, yo!
+
+            //end of answers for help.
+
         }
 
         public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
